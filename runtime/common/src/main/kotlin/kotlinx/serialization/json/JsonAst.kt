d2e6163db8bc2eb455b502afcedf6d3540133b17
Leonid Startsev <sandwwraith@gmail.com>
2018-05-03 19:57:01 +0300

    [JSON-AST] Saving serializable classes to AST

--- runtime/common/src/main/kotlin/kotlinx/serialization/json/JsonAst.kt
+++ runtime/common/src/main/kotlin/kotlinx/serialization/json/JsonAst.kt
@@ -17,6 +17,7 @@
 package kotlinx.serialization.json
 
 import kotlinx.serialization.*
+import kotlinx.serialization.internal.SIZE_INDEX
 import kotlin.reflect.KClass
 
 /**
@@ -42,13 +43,21 @@ sealed class JsonPrimitive : JsonElement() {
  * Represents quoted JSON strings
  */
 data class JsonString(override val content: String): JsonPrimitive() {
-    override fun toString() = buildString { printQuoted(content) }
+    private var quotedString: String? = null
+
+    override fun toString(): String = if (quotedString != null) quotedString!! else {
+        quotedString = buildString { printQuoted(content) }
+        quotedString!!
+    }
 }
 
 /**
  * Represents unquoted JSON primitives (numbers, booleans and null)
  */
 data class JsonLiteral(override val content: String): JsonPrimitive() {
+    constructor(number: Number): this(number.toString())
+    constructor(boolean: Boolean): this(boolean.toString())
+
     override fun toString() = content
 }
 
@@ -147,6 +156,108 @@ class JsonTreeMapper(val context: SerialContext? = null) {
         return JsonTreeInput(obj).read(loader)
     }
 
+    fun <T> writeTree(obj: T, saver: KSerialSaver<T>): JsonElement {
+        lateinit var result: JsonElement
+        val output = JsonTreeOutput { result = it }
+        output.write(saver, obj)
+        return result
+    }
+
+    private abstract inner class AbstractJsonTreeOutput(val nodeConsumer: (JsonElement) -> Unit) : NamedValueOutput() {
+        init {
+            this.context = this@JsonTreeMapper.context
+        }
+
+        override fun composeName(parentName: String, childName: String): String = childName
+
+        abstract fun putElement(key: String, element: JsonElement)
+        abstract fun getCurrent(): JsonElement
+
+        override fun writeTaggedNull(tag: String) = putElement(tag, JsonNull)
+
+        override fun writeTaggedInt(tag: String, value: Int) = putElement(tag, JsonLiteral(value))
+        override fun writeTaggedByte(tag: String, value: Byte) = putElement(tag, JsonLiteral(value))
+        override fun writeTaggedShort(tag: String, value: Short) = putElement(tag, JsonLiteral(value))
+        override fun writeTaggedLong(tag: String, value: Long) = putElement(tag, JsonLiteral(value))
+        override fun writeTaggedFloat(tag: String, value: Float) = putElement(tag, JsonLiteral(value))
+        override fun writeTaggedDouble(tag: String, value: Double) = putElement(tag, JsonLiteral(value))
+        override fun writeTaggedBoolean(tag: String, value: Boolean) = putElement(tag, JsonLiteral(value))
+
+        override fun writeTaggedChar(tag: String, value: Char) = putElement(tag, JsonString(value.toString()))
+        override fun writeTaggedString(tag: String, value: String) = putElement(tag, JsonString(value))
+        override fun <E : Enum<E>> writeTaggedEnum(tag: String, enumClass: KClass<E>, value: E) = putElement(tag, JsonString(value.toString()))
+
+        override fun writeTaggedValue(tag: String, value: Any) {
+            putElement(tag, JsonString(value.toString()))
+        }
+
+        override fun writeBegin(desc: KSerialClassDesc, vararg typeParams: KSerializer<*>): KOutput {
+            val consumer =
+                if (currentTagOrNull == null) nodeConsumer
+                else { node -> putElement(currentTag, node) }
+            return when (desc.kind) {
+                KSerialClassKind.LIST, KSerialClassKind.SET -> JsonTreeListOutput(consumer)
+                KSerialClassKind.MAP -> JsonTreeMapOutput(consumer)
+                KSerialClassKind.ENTRY -> JsonTreeEntryOutput(this@AbstractJsonTreeOutput::putElement)
+                else -> JsonTreeOutput(consumer)
+            }
+        }
+
+        override fun writeFinished(desc: KSerialClassDesc) {
+            nodeConsumer(getCurrent())
+        }
+    }
+
+    private open inner class JsonTreeOutput(nodeConsumer: (JsonElement) -> Unit) :
+        AbstractJsonTreeOutput(nodeConsumer) {
+        private val map: MutableMap<String, JsonElement> = hashMapOf()
+
+        override fun putElement(key: String, element: JsonElement) {
+            map[key] = element
+        }
+
+        override fun getCurrent(): JsonElement = JsonObject(map)
+    }
+
+    private inner class JsonTreeMapOutput(nodeConsumer: (JsonElement) -> Unit) : JsonTreeOutput(nodeConsumer) {
+        override fun shouldWriteElement(desc: KSerialClassDesc, tag: String, index: Int): Boolean = index != SIZE_INDEX
+    }
+
+    private inner class JsonTreeListOutput(nodeConsumer: (JsonElement) -> Unit) : AbstractJsonTreeOutput(nodeConsumer) {
+        private val array: ArrayList<JsonElement> = arrayListOf()
+
+        override fun shouldWriteElement(desc: KSerialClassDesc, tag: String, index: Int): Boolean = index != SIZE_INDEX
+
+        override fun putElement(key: String, element: JsonElement) {
+            val idx = key.toInt() - 1
+            array.add(idx, element)
+        }
+
+        override fun getCurrent(): JsonElement = JsonArray(array)
+    }
+
+    private inner class JsonTreeEntryOutput(val entryConsumer: (String, JsonElement) -> Unit) :
+        AbstractJsonTreeOutput({ throw IllegalStateException("Use entryConsumer instead") }) {
+
+        private lateinit var elem: JsonElement
+        private lateinit var tag: String
+
+        override fun putElement(key: String, element: JsonElement) {
+            if (key != "key") {
+                elem = element
+            } else {
+                check(element is JsonString) { "Expected tag to be JsonString" }
+                tag = (element as JsonString).str
+            }
+        }
+
+        override fun getCurrent(): JsonElement = elem
+
+        override fun writeFinished(desc: KSerialClassDesc) {
+            entryConsumer(tag, elem)
+        }
+    }
+
     private abstract inner class AbstractJsonTreeInput(open val obj: JsonElement): NamedValueInput() {
         init {
             this.context = this@JsonTreeMapper.context
@@ -154,15 +265,18 @@ class JsonTreeMapper(val context: SerialContext? = null) {
 
         override fun composeName(parentName: String, childName: String): String = childName
 
+        private inline fun <reified T: JsonElement> checkCast(obj: JsonElement): T {
+            check(obj is T) { "Expected ${T::class.simpleName} but found ${obj::class.simpleName}" }
+            return obj as T
+        }
 
         override fun readBegin(desc: KSerialClassDesc, vararg typeParams: KSerializer<*>): KInput {
             val curObj = currentTagOrNull?.let { currentElement(it) } ?: obj
-            // todo: more informative exceptions instead of ClassCast
             return when (desc.kind) {
-                KSerialClassKind.LIST, KSerialClassKind.SET -> JsonTreeListInput(curObj as JsonArray)
-                KSerialClassKind.MAP -> JsonTreeMapInput(curObj as JsonObject)
+                KSerialClassKind.LIST, KSerialClassKind.SET -> JsonTreeListInput(checkCast(curObj))
+                KSerialClassKind.MAP -> JsonTreeMapInput(checkCast(curObj))
                 KSerialClassKind.ENTRY -> JsonTreeMapEntryInput(curObj, currentTag)
-                else -> JsonTreeInput(curObj as JsonObject)
+                else -> JsonTreeInput(checkCast(curObj))
             }
         }
 
@@ -211,9 +325,7 @@ class JsonTreeMapper(val context: SerialContext? = null) {
             return READ_DONE
         }
 
-        override fun currentElement(tag: String): JsonElement {
-            return obj.getValue(tag)
-        }
+        override fun currentElement(tag: String): JsonElement = obj.getValue(tag)
 
     }
 
@@ -222,11 +334,9 @@ class JsonTreeMapper(val context: SerialContext? = null) {
         override fun currentElement(tag: String): JsonElement = if (tag == "key") {
             JsonString(cTag)
         } else {
-            check(tag == "value") {"Found unexpected tag: $tag"}
+            check(tag == "value") { "Found unexpected tag: $tag" }
             obj
         }
-
-        override fun readElement(desc: KSerialClassDesc): Int = READ_ALL
     }
 
     private inner class JsonTreeMapInput(override val obj: JsonObject): JsonTreeInput(obj) {
